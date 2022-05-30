2137ce0fbf49d7e3cbaf5d8abec659472a7be830
Nunes-Will <nuneswillroberto@gmail.com>
2022-05-30 15:10:37 -0300

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  Oi
+ 
+  <header>Home | Jogadores | Times</header>
+  <main>Main</main>
+  <footer>Rodapé</footer>
+  
 </template>
 
 <style>
@@ -8,11 +12,34 @@
 #app {
   max-width: 1280px;
   margin: 0 auto;
-  padding: 2rem;
+  /* padding: 2rem; */
+  height: 100vh;
 
   font-weight: normal;
-  background-color: cyan;
 }
+
+header,footer{
+  background-color: crimson;
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+}
+
+main{
+  height: 70%;
+}
+
+/* .conteudo{
+  background-color: cyan;
+  border: 2px solid black;
+  height: 80px;
+  width: 350px;
+  ouverflow: hidden:
+  overflow: scroll;
+}*/
 
 
 </style>
