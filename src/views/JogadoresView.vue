cd49e461ac51073f14ba4c28192a73aef03ad001
Nunes-Will <nuneswillroberto@gmail.com>
2022-06-09 11:40:36 -0300

    Pon

--- src/views/JogadoresView.vue
+++ src/views/JogadoresView.vue
@@ -1,14 +1,25 @@
 <script>
+import {v4 as uuid} from "uuid";
 export default {
   data() {
-    return { 
+    return {
+      novo_jogador: " ", 
       jogadores:[
-        {id: 1, name: "Lionel Messi", afiliacao: "PSG", acao: "6 | 10 | 1"},
-        {id: 2, name: "Cristiano Ronaldo", afiliacao: "Manchester United", acao: "3 | 8 | 0"},
-        {id: 3, name: "Tony Kroos", afiliacao: "Real-Madri", acao: "2 | 12 | 4"},
-        {id: 4, name: "Liverpool", afiliacao: "Mane", acao: "3 | 6 | 3"},
+        {id: "442e6fe2-e800-11ec-8fea-0242ac120002", name: "Lionel Messi", afiliacao: "PSG", acao: "6 | 10 | 1"},
+        {id: "5116842e-e800-11ec-8fea-0242ac120002", name: "Cristiano Ronaldo", afiliacao: "Manchester United", acao: "3 | 8 | 0"},
+        {id: "5b745d2e-e800-11ec-8fea-0242ac120002", name: "Tony Kroos", afiliacao: "Real-Madri", acao: "2 | 12 | 4"},
+        {id: "6043b8ae-e800-11ec-8fea-0242ac120002", name: "Liverpool", afiliacao: "Mane", acao: "3 | 6 | 3"},
       ],
     };
+  },
+  methods: {
+    salvar() {
+      const id = uuid();
+      this.jogadores.push({
+        id: id,
+        name: this.novo_jogador,
+      });
+    },
   },
 };
 </script>
@@ -19,9 +30,9 @@ export default {
       <h2>Histórico jogadores</h2>
     </div>
     <div class="forme-input">
-      <input type="text" placeholder="Nome" />
-      <input type="text" placeholder="Time" />
-      <button>Salvar</button>
+      <input type="text" placeholder="Nome" v-model="novo_jogador"/>
+      <input type="text" placeholder="Time" v-model="time"/>
+      <button @click="salvar">Salvar</button>
     </div>
     <div class="list-jogadores">
       <table>
--- src/views/TimesView.vue
+++ src/views/TimesView.vue
@@ -1,14 +1,25 @@
 <script>
+import {v4 as uuid} from "uuid";
 export default {
   data() {
     return { 
-      times:[
-        {id: 3, name: "Real-Madri", acao: "25 | 6 | 3"},
-        {id: 1, name: "PSG", acao: "24 | 7 | 4"},
-        {id: 4, name: "Manchester United", acao: "24 | 8 | 3"},
-        {id: 2, name: "Liverpool", acao: "22 | 7 | 5"},
+      novo_time: "",
+      times: [
+        {id: "2f320e7b-7ea7-4c21-8e1e-4f1f3982c9ab", name: "Real-Madri", acao: "35 | 25 | 6 | 3"},
+        {id: "5f7ff064-e7fa-11ec-8fea-0242ac120002", name: "PSG", acao: "35 | 24 | 7 | 4"},
+        {id: "691fec32-e7fa-11ec-8fea-0242ac120002", name: "Manchester United", acao: "35 | 24 | 8 | 3"},
+        {id: "71334554-e7fa-11ec-8fea-0242ac120002", name: "Liverpool", acao: "35 | 22 | 7 | 5"},
       ],
     };
+  },
+  methods: {
+    salvar() {
+      const id = uuid();
+      this.times.push({
+        id: id,
+        name: this.novo_time,
+      });
+    },
   },
 };
 </script>
@@ -19,8 +30,8 @@ export default {
       <h2>Gerenciamento de Times</h2>
     </div>
     <div class="forme-input">
-      <input type="text" />
-      <button>Salvar</button>
+      <input type="text" placeholder="Time" v-model="novo_time" />
+      <button @click="salvar">Salvar</button>
     </div>
     <div class="list-times">
       <table>
@@ -28,7 +39,7 @@ export default {
           <tr>
             <th>ID</th>
             <th>Nome</th>
-            <th>V | D | E</th>
+            <th>J | V | D | E</th>
           </tr>
         </thead>
         <tbody>
